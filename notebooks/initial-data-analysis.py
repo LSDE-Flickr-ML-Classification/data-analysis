02bd11bf8fb42a2322b3f4402af0cddbea92b213
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-09-25 23:25:25 +0200

    Add sampling over all files

--- notebooks/initial-data-analysis.py
+++ notebooks/initial-data-analysis.py
@@ -2,7 +2,6 @@
 from pyspark.sql.types import StructField, StringType, IntegerType, StructType
 
 # Initial Data Analysis - STEP 01
-#
 # Load raw CSV of only a portion (can later be extended to full dataset)
 
 # LOAD CSV
@@ -33,24 +32,25 @@ csv_field_list = [
 ]
 csv_data_scheme = StructType(fields=csv_field_list)
 df_full = spark.read.format("CSV").option("delimiter", "\t").schema(csv_data_scheme).load("/mnt/data/flickr/yfcc100m_dataset-1.bz2")
-# df_full.printSchema()
 
 # COMMAND ----------
 
-# Show main metrics on the full_df (possibly sample a subset of the data)
-df_full_count = df_full.count()
+###
+# Sampels the flicker dataset
+###
+def sample_flickr_dataset(full_sample = False):
+  files_list = dbutils.fs.ls("/mnt/data/flickr")
+  DF_SAMPLE_SIZE_FRAC = 0.0001
+  BUCKET_COUNT = (len(files_list) if full_sample else 1) 
 
-print("Amount of rows read: {}".format(df_full_count))
-
-# COMMAND ----------
-
-# Optional STEP - Reduce amount of rows with sampling
-
-# One Portion of the flickr file consists of 10,000,000 records, we sample:
-DF_SAMPLE_SIZE = 1000
-
-df_raw_flickr = df_full.sample(DF_SAMPLE_SIZE / df_full_count)
-df_raw_flickr_count = df_raw_flickr.count() # for later reference
+  df_sampled_buckets = spark.createDataFrame([], csv_data_scheme)
+  print("file | sample size")
+  for file in files_list[0:BUCKET_COUNT]:
+    df_bucket = spark.read.format("CSV").option("delimiter", "\t").schema(csv_data_scheme).load(file.path[5:]).sample(DF_SAMPLE_SIZE_FRAC)
+    print("{0} | {1}".format(file.name, df_bucket.count()))
+    df_sampled_buckets = df_sampled_buckets.union(df_bucket)
+  
+  return df_sampled_buckets
 
 # COMMAND ----------
 
@@ -61,14 +61,35 @@ def col_analysis(col_name, df):
   
   print("Analysis for column: {0}".format(col_name))
   print("\t Total NULL Value : {0}".format(null_values_cnt))
-  print("\t NULL Value Ratio column longitude: {0}".format(null_values_cnt / df_raw_flickr_count))
-  
+  print("\t NULL Value Ratio column longitude: {0}".format(null_values_cnt / col_total_cnt))
 
 # COMMAND ----------
+
+# Sample dataset (set to True if full dataset - 34gb of data!) and write to parquet
+df_sampled = sample_flickr_dataset(False)
+df_sampled.write.mode("overwrite").format("parquet").save("/mnt/group07/flicker_sampled.parquet")
+
+# show folder structure (check)
+dbutils.fs.ls("/mnt/group07/")
+
+# COMMAND ----------
+
+df_raw_flickr = spark.read.parquet("/mnt/group07/flicker_sampled.parquet")
 
 # Analysis by column
 col_analysis("id", df_raw_flickr)
 col_analysis("longitude", df_raw_flickr)
+
+# TODO:
+# check possible null values
+# data violations defined by the schema
+# date fields: date_taken, date_uploaded
+# url fields: photo/video page url / photo/video download url, liecense url
+# 
+# is data plausible:
+# -> is picture really, picture, video really video?
+
+# check if picture exists - only request head
 
 # COMMAND ----------
 
